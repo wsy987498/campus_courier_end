6df7f7f0f3a811169ace1d6c3387e3224285c634
wsy987498 <wusiyu0328@163.com>
2021-03-24 15:04:39 +0800

    update admin

--- router/admin/index.js
+++ router/admin/index.js
@@ -2,17 +2,16 @@
 const Router = require("koa-router")
 const router = new Router()
 const login = require("./login")
-const users = require("./users")
+
 
 router.get("/", async (ctx) => {
-  ctx.body = "hellow Koa2!" // 返回数据给页面 ctx.response.body="xxx" === ctx.body="xxx"
+  ctx.body = "hellow Koa2!"
 })
 
 router.get("/admin", async (ctx) => {
   ctx.body = "admin index page!"
 })
 
-router.use("/mobile", users.routes(), users.allowedMethods())
 router.use("/admin", login.routes(), login.allowedMethods())
 
 //exports  
