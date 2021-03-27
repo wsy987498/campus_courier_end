35b5713604561af287dacb93ce513179501e8125
wsy987498 <wusiyu0328@163.com>
2021-03-27 23:40:01 +0800

    edit api

--- router/mobile/waitOrder.js
+++ router/mobile/waitOrder.js
@@ -61,4 +61,35 @@ waitOrder.post("/delmytask", async (ctx) => {
   ctx.body = res
 })
 
+// update编辑发布订单
+waitOrder.post("/updateorder", async (ctx) => {
+  const express_name = ctx.request.body.express_name
+  const express_type = ctx.request.body.express_type
+  const forward_delivery_time = ctx.request.body.forward_delivery_time
+  const express_recipients = ctx.request.body.express_recipients
+  const express_money = ctx.request.body.express_money
+  const delivery_address = ctx.request.body.delivery_address
+  const phone = ctx.request.body.phone
+  const pick_code = ctx.request.body.pick_code
+  const remarks = ctx.request.body.remarks
+  const express_id = ctx.request.body.express_id
+
+  const updateSql = `update express_list set 
+  
+  express_name='${express_name}',express_type='${express_type}', forward_delivery_time='${forward_delivery_time}',express_recipients='${express_recipients}',express_money='${express_money}',delivery_address='${delivery_address}',phone='${phone}',pick_code='${pick_code}',remarks='${remarks}' where express_id='${express_id}'`
+
+  const res = await new Promise((resolve, reject) => {
+    return db.query(updateSql, (err, data) => {
+      if (err) throw err
+      let obj = {
+        code: 200,
+        msg: '修改成功'
+      }
+      resolve(obj)
+    })
+  })
+  ctx.body = res
+})
+
+
 module.exports = waitOrder
